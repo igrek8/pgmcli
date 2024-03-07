d453c9cb1432ca1ed5b7bb1c6b66dffa21eae420
Igor Korchagin <7078731+igrek8@users.noreply.github.com>
2024-03-07 17:07:48 +0100

    fix: parse timestamp in applied migrations

--- src/core/get-applied-migrations.ts
+++ src/core/get-applied-migrations.ts
@@ -6,9 +6,9 @@ import { sort } from "./sort.js";
 export async function getAppliedMigrations(client: pg.Client, table: string) {
   return new Map<string, Migration>(
     (await client.query<Migration>(`SELECT * FROM ${table}`)).rows
-      .map<[string, Migration]>(({ id, created_at, meta }) => {
-        const timestamp = Number.parseInt(id);
-        assert(Number.isFinite(timestamp));
+      .map<[string, Migration]>(({ id, meta }) => {
+        const created_at = Number.parseInt(id);
+        assert(Number.isFinite(created_at));
         return [id, { id, created_at, meta }];
       })
       .sort(([, a], [, b]) => sort(a, b)),
